04453040d7eab68d366a4bb434716c92a9255c35
Giovane <69176083+GiovaneFGC@users.noreply.github.com>
2023-04-14 17:42:52 -0300

    Add files via upload
    mega sena!!!!!

---
+++ mega.py
@@ -0,0 +1,12 @@
+import random
+while True:
+    a = int(input('quantos jogos?'))
+    if a == 0:
+        quit()
+    for a in range (1,a+1):
+        print ('jogo', a, ":")
+        num = " "
+        for b in range (1,16):
+            r = random.randint(1900,2001)
+            num = num+" "+str(r)
+        print (num) 
